0157d4dfcefd186694b65a4da007d2e5a99ad903
Joe Zuntz <joezuntz@googlemail.com>
2021-07-16 13:10:55 +0100

    remove old comment

--- qp/version.py
+++ qp/version.py
@@ -1,7 +1,6 @@
 def find_version():
     # setuptools_scm should install a
     # file _version alongside this one.
-    # This is the fastest method
     from . import _version
     return _version.version
 
