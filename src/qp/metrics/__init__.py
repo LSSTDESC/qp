0bcc1ab6017bfb718b6de186ae075fd48f86010f
Eric Charles <badass@stanford.edu>
2023-11-21 11:35:46 -0800

    Added metric factory

--- src/qp/metrics/__init__.py
+++ src/qp/metrics/__init__.py
@@ -5,3 +5,11 @@ from .goodness_of_fit import *
 # added for testing purposes
 from .metrics import _calculate_grid_parameters, _check_ensemble_is_not_nested, _check_ensembles_are_same_size
 from .metrics import _check_ensembles_contain_correct_number_of_distributions
+
+from .factory import MetricFactory
+
+
+create_metric = MetricFactory.create_metric
+print_metrics = MetricFactory.print_metrics
+list_metrics = MetricFactory.list_metrics
+update_metrics = MetricFactory.update_metrics
