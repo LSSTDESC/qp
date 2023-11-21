d180ef6072148ed9cc7b22192d203cca79af15db
Eric Charles <badass@stanford.edu>
2023-11-21 13:41:46 -0800

    added base_metric_classes to interface in __init__

--- src/qp/metrics/__init__.py
+++ src/qp/metrics/__init__.py
@@ -1,8 +1,10 @@
 from .array_metrics import *
 from .metrics import *
 from .goodness_of_fit import *
+from .base_metric_classes import *
 
 # added for testing purposes
+
 from .metrics import _calculate_grid_parameters, _check_ensemble_is_not_nested, _check_ensembles_are_same_size
 from .metrics import _check_ensembles_contain_correct_number_of_distributions
 
